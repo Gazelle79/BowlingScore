5b9b6cf2f16815e5c4fbb65a4830b835a53411f3
Gazelle79 <fboydjr@gmail.com>
2022-09-15 12:10:33 -0400

    Renaming directory structure to account for proper namespaces. src/com/BowlingScore is now src/main/java/com/bowlingscore. Saving.

---
+++ src/main/java/com/bowlingscore/businesslogic/BowlingScoreCalculator.java
@@ -0,0 +1,299 @@
+package com.bowlingscore.businesslogic;
+
+import java.io.*;
+
+public class BowlingScoreCalculator 
+{	
+	private final int maxFrames = 10;
+	private final int maxTriesPerFrame = 2;
+	
+	/** Scoring Rules
+	    X = STRIKE (10 + total number of pins knocked down in next two rolls, on next turn)
+		- = MISS (0)
+		[/] = SPARE (10 + total number of pins knocked down in next roll, on next turn)
+		[0-9] = Total number of pins knocked down on this turn
+	 **/
+	
+	/**
+	 * Reads a bowling score from a text file.
+	 * */
+	public String readBowlingScore(String fileNameAndPath) throws IOException
+	{
+		BufferedReader reader = null;
+		String bowlingScoreText = null;
+		StringBuilder contents = new StringBuilder();
+				
+			/*Not put in try / catch statement so error could be sent back to method caller.*/
+			InputStream is = new FileInputStream(fileNameAndPath);
+			reader = new BufferedReader(new InputStreamReader(is));
+			
+			while ((bowlingScoreText = reader.readLine()) != null) 
+			{
+				contents.append(bowlingScoreText + "\n");
+			}
+			reader.close();
+				
+		return contents.toString();
+	}
+	
+	public int getNumberOfBowlingScores(String fileNameAndPath) throws IOException
+	{
+		int numberOfGames = 0;
+		
+		BufferedReader reader = null;
+		
+			InputStream is = new FileInputStream(fileNameAndPath);
+			reader = new BufferedReader(new InputStreamReader(is));
+			
+			while ((reader.readLine()) != null) 
+			{
+				numberOfGames++;
+			}
+			reader.close();
+		
+		return numberOfGames;
+	}
+	
+	/**
+	 * Calculates scoring per frame. A player has two tries in a frame.
+	 * Both of those scores are recorded and added in an individual frame score. 
+	 * */
+	public int calculateBowlingScore(String rawBowlingScore)
+	{				
+		int[] frames = new int[maxFrames]; //Allocates an array of 10 frames.
+		int finalScore = 0;
+		int frameScore = 0;
+		int frameindex = 0; //index used for the array of frames.
+		
+		char currentChar = ' ';
+		char secondChar = ' ';
+		char thirdChar = ' ';
+		
+		
+		int tries = 0; //2 tries = 1 frame.
+		
+		//You have 10 frames. Put the score for that frame in an array.
+		//Add all scores in the frames, and you're done.
+		
+		for(int i=0; i < rawBowlingScore.length()-1; i++)
+		{
+			currentChar = rawBowlingScore.charAt(i);
+			if(currentChar == '-')
+			{
+				frameScore += this.getRollScore(currentChar);
+				tries++;
+				if(tries == maxTriesPerFrame) //This frame is done with 2 tries.
+				{
+					//Put frameScore in frames[i].
+					frames[frameindex] = frameScore;
+					System.out.println("Frame " + frameindex + " score: " + frameScore);
+					frameindex++;
+					
+					//reset Tries and Framescore to 0.
+					tries = 0;
+					frameScore = 0;
+				}
+			}
+			else if(this.isNumeric(currentChar))
+			{
+				frameScore += this.getRollScore(currentChar);
+				tries++;
+				if(tries == maxTriesPerFrame) //This frame is done with 2 tries.
+				{
+					frames[frameindex] = frameScore;
+					System.out.println("Frame " + frameindex + " score: " + frameScore);
+					frameindex++;
+					tries = 0;
+					frameScore = 0;
+				}
+			}
+			else if(currentChar == '/')
+			{
+				//Score is 10 + pins hit in next throw.
+				secondChar = rawBowlingScore.charAt(i+1);
+				frameScore = this.getRollScore(currentChar) + this.getRollScore(secondChar);
+				tries = 2;
+				if(tries == maxTriesPerFrame) //This frame is done with 2 tries.
+				{
+					frames[frameindex] = frameScore;
+					System.out.println("Frame " + frameindex + " score: " + frameScore);
+					frameindex++;
+					tries = 0;
+					frameScore = 0;
+				}
+			}
+			else if(currentChar == 'X')
+			{
+				//Score is 10 + pins hit in next TWO throws.
+				secondChar = rawBowlingScore.charAt(i+1);
+				thirdChar = rawBowlingScore.charAt(i+2);
+				frameScore = this.getRollScore(currentChar) + this.getRollScore(secondChar) + this.getRollScore(thirdChar);
+				tries = 2;
+				if(tries == maxTriesPerFrame) //This frame is done with 2 tries.
+				{
+					frames[frameindex] = frameScore;
+					System.out.println("Frame " + frameindex + " score: " + frameScore);
+					frameindex++;
+					tries = 0;
+					frameScore = 0;
+				}
+			}
+			if(frameindex == maxFrames)
+			{ break; }
+		}
+		
+		//Frames[] is an array of scores for each frame. 
+		//Add all the scores for the final score.
+		for( int singleFrameScore : frames) 
+		{
+		    finalScore += singleFrameScore;
+		}
+		
+		System.out.println("\nFinal score for this game: " + finalScore + "\n");
+		return finalScore;
+	}	
+
+	
+	/**
+	 * Returns the score a user got on his first or second roll in a frame. 
+	 * */
+	private int getRollScore(char charToTranslate)
+	{
+		int rollScore = 0;
+		if(this.isNumeric(charToTranslate)) //Standard hit. 0-9 pins.
+		{
+			rollScore = Character.getNumericValue(charToTranslate);
+		}
+		else if(charToTranslate == '-') //Miss
+		{
+			rollScore = 0;
+		}
+		else if(charToTranslate == 'X') //Strike
+		{
+			rollScore = 10;
+		}
+		else if(charToTranslate == '/') //Spare
+		{
+			rollScore = 10;
+		}
+		else
+		{
+			rollScore = rollScore + 0;
+		}
+		
+		return rollScore;
+	}
+	
+	/**
+	 * Sees is the character read from the bowling string unput is numeric [0-9].
+	 * */
+	private boolean isNumeric(char str)  
+	{ 
+		boolean isNumeric = false;
+		try  
+		{  
+			int character = Integer.parseInt(Character.toString(str));
+			isNumeric = true;
+		}  
+		catch(NumberFormatException e)  
+		{  
+			isNumeric = false;  
+		}  
+	  return isNumeric;  
+	}
+	
+	/**
+	 * Returns the ACTUAL number of frames in a bowling input stream. 
+	 * Accounts for ANY combination of strikes, spares, misses, etc.
+	 * Thought I would need this method, but I won't.
+	 * */
+	private int getFrameCount(String rawBowlingScore)
+	{
+		int frameCount = 0;
+		char currentChar = ' ';
+		int charCount = 0;
+		
+		for(int i=0; i<rawBowlingScore.length()-1; i++)
+		{
+			currentChar = rawBowlingScore.charAt(i);
+			if(currentChar == 'X' && frameCount < maxFrames)
+			{
+				frameCount+=1;
+			}
+			else
+			{
+				charCount +=1;
+				if(charCount %2 == 0 && frameCount < maxFrames)
+				{
+					frameCount +=1 ;
+					charCount = 0;
+				}
+			}
+		}
+		
+		return frameCount;
+	}
+	
+	/**
+	 * Write ONE bowling score to an output file.
+	 * */
+	public boolean writeBowlingScore(int finalScore, String fileNameAndPath)
+	{
+		boolean wasWrittenToFile = false;
+
+			try 
+			{
+				File businessCardFile = new File(fileNameAndPath);
+				
+				if(businessCardFile.exists())
+				{
+					businessCardFile.delete();
+				}
+				
+				FileWriter writer = new FileWriter(businessCardFile);				
+				writer.write("Final bowling score: " + finalScore);
+				writer.close();
+				wasWrittenToFile = true;								
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+		
+		return wasWrittenToFile;
+	}
+	
+	
+	/**
+	 * Write MANY bowling scores to an output file.
+	 * */
+	public boolean writeBowlingScores(int[] finalScores, String fileNameAndPath)
+	{
+		boolean wasWrittenToFile = false;
+
+			try 
+			{
+				File businessCardFile = new File(fileNameAndPath);
+				
+				if(businessCardFile.exists())
+				{
+					businessCardFile.delete();
+				}
+				
+				FileWriter writer = new FileWriter(businessCardFile);
+				for(int i=0; i<= finalScores.length-1; i++)
+				{
+					writer.write("Game " + i + " final score: " + finalScores[i] + "\n");
+				}	
+				writer.close();
+				wasWrittenToFile = true;								
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+		
+		return wasWrittenToFile;
+	}
+	
+}
---
+++ src/main/java/com/bowlingscore/businesslogic/BowlingScoreMain.java
@@ -0,0 +1,56 @@
+package com.bowlingscore.businesslogic;
+
+import java.io.File;
+
+public class BowlingScoreMain 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int[] bowlingScores = new int[0];
+		String currentPath = new File("").getAbsolutePath();		
+		
+		String inputFileNameAndPath =  currentPath + "/InputFiles/BowlingScoreInput.txt";
+		String outputFileNameAndPath = currentPath + "/OutputFiles/BowlingScoreOutput.txt";
+		String[] rawBowlingScores = new String[0]; //blank strings
+		boolean wasWrittenToFile = false;
+		
+		if(args.length > 1)
+		{
+			inputFileNameAndPath =  args[0];
+			outputFileNameAndPath = args[1];
+		}
+		
+		BowlingScoreCalculator parser = new BowlingScoreCalculator();
+		
+		
+		try
+		{
+			int bowlingScoreCount = parser.getNumberOfBowlingScores(inputFileNameAndPath);
+			rawBowlingScores = new String[bowlingScoreCount];
+			bowlingScores = new int[bowlingScoreCount];
+			
+			//Read the bowling score in. (Assume it's always valid)
+			//Calculate the score.
+			//Write that score to a text file.
+			rawBowlingScores = parser.readBowlingScore(inputFileNameAndPath).split("\n");
+			
+			
+			for(int i=0; i<bowlingScoreCount; i++)
+			{
+				bowlingScores[i] = parser.calculateBowlingScore(rawBowlingScores[i]);
+			}
+			wasWrittenToFile = parser.writeBowlingScores(bowlingScores, outputFileNameAndPath);
+		}
+		
+		catch (Exception e)
+		{
+			System.out.println("Something went wrong: " + e.getMessage());
+		}
+		
+		
+		
+	}
+
+}
